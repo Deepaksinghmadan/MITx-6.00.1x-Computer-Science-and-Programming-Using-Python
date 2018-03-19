a1acb9831c7ca4c5acfe6832fa6e1a4613c5ad76
Deepak <deepakms.real@gmail.com>
2018-03-19 21:52:49 +0530

    Add files via upload

---
+++ Invered star.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 21:01:40 2018
+
+@author: deepak
+"""
+
+a=int(input("enter the number of rows "))
+for i in range(a,0,-1):
+    print((a-i)*" "+i*"*")
+    
