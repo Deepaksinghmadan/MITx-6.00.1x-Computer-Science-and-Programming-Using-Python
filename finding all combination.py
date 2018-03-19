4c7ef7bbf6c293a0b8345d76f8fd967f0dce607c
Deepak <deepakms.real@gmail.com>
2018-03-19 21:54:54 +0530

    Add files via upload

---
+++ finding all combination.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Mar 19 19:34:12 2018
+
+@author: deepak
+"""
+
+a=int(input("Enter first number:"))
+b=int(input("Enter second number:"))
+c=int(input("Enter third number:"))
+d=[]
+d.append(a)
+d.append(b)
+d.append(c)
+for i in range(0,3):
+    for j in range(0,3):
+        for k in range(0,3):
+            if(i!=j&j!=k&k!=i):
+                print(d[i],d[j],d[k])
